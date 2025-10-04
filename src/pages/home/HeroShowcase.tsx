6716ec550602bca8d0c1a140051991214d45ab44
txmlrd <gungadhisanjaya@gmail.com>
2025-10-04 21:40:24 +0700

    feat: updating landing page

--- src/pages/home/HeroShowcase.tsx
+++ src/pages/home/HeroShowcase.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ThreeDMarquee } from "@/components/ui/3d-marquee";
+import { SparklesTitle } from "./SparkleTitle";
 
 export default function HeroShowcase() {
   const images = [
@@ -41,8 +42,17 @@ export default function HeroShowcase() {
     "https://tomvisual.vercel.app/images/project/starter.png",
   ];
   return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <ThreeDMarquee images={images} />
+    <div className="relative h-screen w-full overflow-hidden">
+      {/* Background marquee */}
+      <ThreeDMarquee className="absolute inset-0 z-0" images={images} />
+
+      {/* Overlay hitam */}
+      <div className="absolute inset-0 bg-slate-950/40 z-0"></div>
+
+      {/* Konten tulisan */}
+      <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0 z-10 flex flex-col items-center justify-center h-screen text-center">
+        <SparklesTitle />
+      </div>
     </div>
   );
 }
